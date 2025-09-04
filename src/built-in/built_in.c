3e2e9a8f3d16926c6845604a7bed73b9f82ed797
Armand Mosnier <armosnie@f1r4s20.clusters.42paris.fr>
2025-09-04 15:27:55 +0200

    fix : redir of built-in with outfile

--- src/built-in/built_in.c
+++ src/built-in/built_in.c
@@ -6,12 +6,21 @@
 /*   By: armosnie <armosnie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/28 13:48:50 by armosnie          #+#    #+#             */
-/*   Updated: 2025/09/04 14:05:16 by armosnie         ###   ########.fr       */
+/*   Updated: 2025/09/04 15:22:13 by armosnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/exec.h"
 #include "../../includes/minishell.h"
+
+int	redir_and_restore_stdout(t_cmd *cmd)
+{
+	int	prev_old_fd;
+
+	prev_old_fd = dup(FD_STDOUT);
+	open_outfile(cmd);
+	return (prev_old_fd);
+}
 
 bool	is_built_in(t_cmd *cmd)
 {
@@ -36,13 +45,13 @@ bool	is_built_in(t_cmd *cmd)
 
 int	parent_process_built_in(t_cmd *cmd, t_env *env)
 {
-    int code_error;
+	int	code_error;
+	int	prev_old_fd;
 
-    code_error = 0;
+	code_error = 0;
+	prev_old_fd = -1;
 	if (cmd->outfile && cmd->outfile->name)
-	{
-		open_outfile(cmd);
-	}
+		prev_old_fd = redir_and_restore_stdout(cmd);
 	if (ft_strcmp(cmd->name, "echo") == 0)
 		code_error = built_in_echo(cmd);
 	if (ft_strcmp(cmd->name, "cd") == 0)
@@ -52,19 +61,21 @@ int	parent_process_built_in(t_cmd *cmd, t_env *env)
 	if (ft_strcmp(cmd->name, "export") == 0)
 		code_error = built_in_export(cmd, env, code_error);
 	if (ft_strcmp(cmd->name, "unset") == 0)
-		code_error =  built_in_unset(cmd, env);
+		code_error = built_in_unset(cmd, env);
 	if (ft_strcmp(cmd->name, "env") == 0)
 		code_error = built_in_env(cmd, env);
 	if (ft_strcmp(cmd->name, "exit") == 0)
-	    code_error = built_in_exit(cmd, code_error, env);
-    return (code_error);
+		code_error = built_in_exit(cmd, code_error, env);
+	if (prev_old_fd != -1)
+		return (dup2(prev_old_fd, FD_STDOUT), close(prev_old_fd), code_error);
+	return (code_error);
 }
 
 int	child_process_built_in(t_cmd *cmd, t_env *env)
 {
-    int code_error;
+	int	code_error;
 
-    code_error = 0;
+	code_error = 0;
 	if (ft_strcmp(cmd->name, "echo") == 0)
 		code_error = built_in_echo(cmd);
 	if (ft_strcmp(cmd->name, "cd") == 0)
@@ -74,8 +85,8 @@ int	child_process_built_in(t_cmd *cmd, t_env *env)
 	if (ft_strcmp(cmd->name, "export") == 0)
 		code_error = built_in_export(cmd, env, code_error);
 	if (ft_strcmp(cmd->name, "unset") == 0)
-		code_error =  built_in_unset(cmd, env);
+		code_error = built_in_unset(cmd, env);
 	if (ft_strcmp(cmd->name, "env") == 0)
 		code_error = built_in_env(cmd, env);
-    return (code_error);
+	return (code_error);
 }
