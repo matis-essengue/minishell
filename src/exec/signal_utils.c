1801eb87332bfc0b75052715e368dcbacf9f1d3d
Armand Mosnier <armosnie@f4r7s8.clusters.42paris.fr>
2025-09-09 15:17:25 +0200

    pull main merge

--- src/exec/signal_utils.c
+++ src/exec/signal_utils.c
@@ -6,7 +6,7 @@
 /*   By: armosnie <armosnie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/04 15:06:05 by matis             #+#    #+#             */
-/*   Updated: 2025/09/09 15:16:52 by armosnie         ###   ########.fr       */
+/*   Updated: 2025/09/09 15:17:20 by armosnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,18 +33,11 @@ void	handle_sigint_in_exec(int signal)
 
 void	handle_sigquit(int signal)
 {
-<<<<<<< HEAD:src/signal_utils.c
 	struct sigaction act;
 	
 	ft_bzero(&act, sizeof(act));
 	act.sa_handler = signal_handler;
 	sigaction(SIGINT, &act, NULL);
 	sigaction(SIGQUIT, &act, NULL);
-=======
-	(void)signal;
-	ft_putstr_fd("Quit (core dumped)\n", STDERR_FILENO);
-	// rl_replace_line("", 0);
-	rl_on_new_line();
->>>>>>> 02bd22566fa97471f156c4d49d9b364450b7b1ce:src/exec/signal_utils.c
 }
 
